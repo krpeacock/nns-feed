bd34048a94fd9f725fc817c14753b11c63769894
Kyle Peacock <kylpeacock@gmail.com>
2021-06-17 12:10:57 -0700

    rss feed config working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+const JSONbig = require("json-bigint")
 /**
  * Configure your Gatsby site with this file.
  *
@@ -5,5 +6,75 @@
  */
 
 module.exports = {
-  plugins: [require.resolve(`./plugins/source-plugin`), `gatsby-plugin-feed`],
+  siteMetadata: {
+    title: `NNS Proposal Feed`,
+    siteUrl: `https://peacock.dev/nns-feed`,
+  },
+  plugins: [
+    require.resolve(`./plugins/source-plugin`),
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+      {
+        proposals: allProposal {
+          edges {
+            node {
+              id
+              internal {
+                content
+              }
+            }
+          }
+          distinct(field: id)
+        }
+        site {
+          siteMetadata {
+            title
+            siteUrl
+            site_url: siteUrl
+          }
+        }
+      }
+    `,
+        feeds: [
+          {
+            serialize: ({ query: { site, proposals } }) => {
+              return proposals.edges.map(edge => {
+                const proposal = JSONbig.parse(edge.node.internal.content)
+                const id = proposal.id[0].id.toString()
+                proposal.id = id
+                proposal.slug = `/proposals/${id}`
+
+                return {
+                  description: proposal.proposal.summary,
+                  date: proposal.proposal_timestamp_seconds,
+                  url: site.siteMetadata.siteUrl + "/proposals/" + proposal.id,
+                  guid: site.siteMetadata.siteUrl + proposal.slug,
+                }
+              })
+            },
+            query: `
+            {
+              proposals: allProposal {
+                edges {
+                  node {
+                    id
+                    internal {
+                      content
+                    }
+                  }
+                }
+                distinct(field: id)
+              }
+            }
+          `,
+            output: "/rss.xml",
+            title: "NNS Proposals",
+            match: "^/proposals/",
+          },
+        ],
+      },
+    },
+  ],
 }
